3b2245ca7c65c5947fd272d3dc09d48a2c93ae71
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-10-07 14:54:02 -0400

    LIBFCREPO-144. Code cleanup.
    https://issues.umd.edu/browse/LIBFCREPO-144

--- pcdm2manifest.rb
+++ pcdm2manifest.rb
@@ -13,6 +13,19 @@ FCREPO_BASE_URI = config['fcrepo_base_uri']
 IIIF_IMAGE_URI = config['iiif_image_uri']
 IIIF_MANIFEST_URI = config['iiif_manifest_uri']
 
+
+#RDF METADATA KEYS
+DC_TITLE = 'http://purl.org/dc/elements/1.1/title'
+DC_DATE = 'http://purl.org/dc/elements/1.1/date'
+BIBO_EDITION = 'http://purl.org/ontology/bibo/edition'
+BIBO_ISSUE = 'http://purl.org/ontology/bibo/issue'
+BIBO_VOLUME = 'http://purl.org/ontology/bibo/volume'
+IANA_FIRST = 'http://www.iana.org/assignments/relation/first'
+IANA_LAST = 'http://www.iana.org/assignments/relation/last'
+HAS_MEMBER = 'http://pcdm.org/models#hasMember'
+HAS_FILE = 'http://pcdm.org/models#hasFile'
+MIME_TYPE_URI = 'http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#hasMimeType'
+
 issue_uri = ARGV[0]
 
 @fcrepo_conn = Faraday.new(:ssl => { ca_file: config['server_cert'] }) do |faraday|
@@ -32,11 +45,11 @@ def get(uri)
 end
 
 def get_described_by_link_header(uri)
-  links = @fcrepo_conn.head(uri).headers["link"]
+  links = @fcrepo_conn.head(uri).headers['link']
   links_array = LinkHeader.parse(links).to_a
   links_array.each do |link_item|
     link_item[1].each do |key_val_pair|
-      if (key_val_pair[0] == "rel" && key_val_pair[1] == "describedby")
+      if (key_val_pair[0] == 'rel' && key_val_pair[1] == 'describedby')
         return link_item[0]
       end
     end
@@ -45,12 +58,11 @@ def get_described_by_link_header(uri)
 end
 
 def get_metadata_value(metadata, key)
-  metadata[key][0]["@value"]
+  metadata[key][0]['@value']
 end
 
 def get_mime_type(metadata)
-  mime_type_uri = "http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#hasMimeType"
-  get_metadata_value(metadata, mime_type_uri)
+  get_metadata_value(metadata, MIME_TYPE_URI)
 end
 
 def get_path_from_uri(uri)
@@ -62,10 +74,10 @@ end
 def get_image_dimensions(image_uri)
   dimensions = {}
   iiif_image_uri = IIIF_IMAGE_URI + get_path_from_uri(image_uri)
-  info_uri = iiif_image_uri + "/info.json"
+  info_uri = iiif_image_uri + '/info.json'
   info = @iiif_conn.get(info_uri).body
-  dimensions["height"] = info["height"]
-  dimensions["width"] = info["width"]
+  dimensions['height'] = info['height']
+  dimensions['width'] = info['width']
   return dimensions
 end
 
@@ -79,9 +91,9 @@ end
 
 def get_canvas_dimension(image_dimensions)
   canvas_dimensions = image_dimensions.dup
-  if ((canvas_dimensions["height"] < 1200) || (canvas_dimensions["width"] < 1200))
-    canvas_dimensions["height"] = canvas_dimensions["height"] * 2
-    canvas_dimensions["width"] = canvas_dimensions["width"] * 2
+  if ((canvas_dimensions['height'] < 1200) || (canvas_dimensions['width'] < 1200))
+    canvas_dimensions['height'] = canvas_dimensions['height'] * 2
+    canvas_dimensions['width'] = canvas_dimensions['width'] * 2
   end
   return canvas_dimensions
 end
@@ -90,133 +102,133 @@ end
 # Template for generating manifests
 @manifest_template = {
   # Metadata about this manifest file
-  "@context" => "http://iiif.io/api/presentation/2/context.json",
-  "@id" => "http://example.org/iiif/book1/manifest", # Issue Manifest ID
-  "@type" => "sc:Manifest",
+  '@context' => 'http://iiif.io/api/presentation/2/context.json',
+  '@id' => 'http://example.org/iiif/book1/manifest', # Issue Manifest ID
+  '@type' => 'sc:Manifest',
 
   # Descriptive metadata about the object/work
-  "label" => "Book 1", # Issue Title
-  "metadata" => [  # Issue Meta
-    {"label" => "Author", "value": "Anne Author"},
-    {"label" => "Published", "value": [
-      {"@value" => "Paris, circa 1400", "@language": "en"},
-      {"@value" => "Paris, environ 1400", "@language": "fr"}
+  'label' => 'Book 1', # Issue Title
+  'metadata' => [  # Issue Meta
+    {'label' => 'Author', 'value': 'Anne Author'},
+    {'label' => 'Published', 'value': [
+      {'@value' => 'Paris, circa 1400', '@language': 'en'},
+      {'@value' => 'Paris, environ 1400', '@language': 'fr'}
     ]
     },
-    {"label" => "Notes", "value": ["Text of note 1", "Text of note 2"]},
-    {"label" => "Source",
-     "value" => "<span>From: <a href=\"http://example.org/db/1.html\">Some Collection</a></span>"}
+    {'label' => 'Notes', 'value': ['Text of note 1', 'Text of note 2']},
+    {'label' => 'Source',
+     'value' => '<span>From: <a href=\'http://example.org/db/1.html\'>Some Collection</a></span>'}
   ],
-  "description" => "  ", # Issue Description
-  "thumbnail" => { 
-    "@id" => "http://example.org/images/book1-page1/full/80,100/0/default.jpg", # Issue First Page Image Thumbnail
-    "service" => {
-      "@context" => "http://iiif.io/api/image/2/context.json",
-      "@id" => "http://example.org/images/book1-page1/", # Base URL of the image (i.e. without '/full/80,100/0/default.jpg')
-      "profile" => "http://iiif.io/api/image/2/level1.json"
+  'description' => '  ', # Issue Description
+  'thumbnail' => { 
+    '@id' => 'http://example.org/images/book1-page1/full/80,100/0/default.jpg', # Issue First Page Image Thumbnail
+    'service' => {
+      '@context' => 'http://iiif.io/api/image/2/context.json',
+      '@id' => 'http://example.org/images/book1-page1/', # Base URL of the image (i.e. without '/full/80,100/0/default.jpg')
+      'profile' => 'http://iiif.io/api/image/2/level1.json'
     }
   },
 
   # Presentation Information
-  "viewingDirection" => "right-to-left",
-  "viewingHint" => "paged",
-  "navDate" => "1856-01-01T00:00:00Z",
+  'viewingDirection' => 'right-to-left',
+  'viewingHint' => 'paged',
+  'navDate' => '1856-01-01T00:00:00Z',
 
   # Rights Information
-  "license" => "http://example.org/license.html",
-  "attribution" => "Provided by Example Organization",
+  'license' => 'http://example.org/license.html',
+  'attribution' => 'Provided by Example Organization',
 
-  "logo" => {
-    "@id" => "http://wwwdev.lib.umd.edu/images/wrapper/liblogo.png",
-    "service" => {
-      "@context" => "http://iiif.io/api/image/2/context.json",
-      "@id" => "http://example.org/service/inst1", # IIIF Base URL of the logo
-      "profile" => "http://iiif.io/api/image/2/profiles/level2.json"
+  'logo' => {
+    '@id' => 'http://wwwdev.lib.umd.edu/images/wrapper/liblogo.png',
+    'service' => {
+      '@context' => 'http://iiif.io/api/image/2/context.json',
+      '@id' => 'http://example.org/service/inst1', # IIIF Base URL of the logo
+      'profile' => 'http://iiif.io/api/image/2/profiles/level2.json'
     }
   },
 
   # Links
-  "related":{
-    "@id" => "http://example.org/videos/video-book1.mpg",
-    "format" => "video/mpeg"
+  'related':{
+    '@id' => 'http://example.org/videos/video-book1.mpg',
+    'format' => 'video/mpeg'
   },
-  "service" => {
-    "@context" => "http://example.org/ns/jsonld/context.json",
-    "@id" => "http://example.org/service/example",
-    "profile" => "http://example.org/docs/example-service.html"
+  'service' => {
+    '@context' => 'http://example.org/ns/jsonld/context.json',
+    '@id' => 'http://example.org/service/example',
+    'profile' => 'http://example.org/docs/example-service.html'
   },
-  "seeAlso" => {
-    "@id" => "http://example.org/library/catalog/book1.xml",
-    "format" => "text/xml",
-    "profile" => "http://example.org/profiles/bibliographic"
+  'seeAlso' => {
+    '@id' => 'http://example.org/library/catalog/book1.xml',
+    'format' => 'text/xml',
+    'profile' => 'http://example.org/profiles/bibliographic'
   },
-  "rendering" => {
-    "@id" => "http://example.org/iiif/book1.pdf",
-    "label" => "Download as PDF",
-    "format" => "application/pdf"
+  'rendering' => {
+    '@id' => 'http://example.org/iiif/book1.pdf',
+    'label' => 'Download as PDF',
+    'format' => 'application/pdf'
   },
-  "within" => "http://example.org/collections/books/",
+  'within' => 'http://example.org/collections/books/',
 
   # List of sequences
-  "sequences" => [
+  'sequences' => [
     {
-      "@id" => "http://example.org/iiif/book1/sequence/normal", # Derefrencing optionale 
-      "@type" => "sc:Sequence",
-      "label" => "Current Page Order",
+      '@id' => 'http://example.org/iiif/book1/sequence/normal', # Derefrencing optionale 
+      '@type' => 'sc:Sequence',
+      'label' => 'Current Page Order',
       # sequence's page order should be included here, see below...
       #
-      "viewingDirection" => "left-to-right",
-      "viewingHint" => "paged",
-      "startCanvas" => "http://example.org/iiif/book1/canvas/p2", # Default can be Page 1 Canvas 
+      'viewingDirection' => 'left-to-right',
+      'viewingHint' => 'paged',
+      'startCanvas' => 'http://example.org/iiif/book1/canvas/p2', # Default can be Page 1 Canvas 
 
       # The order of the canvases
-      "canvases" => [
+      'canvases' => [
         {
-          "@id" => "http://example.org/iiif/book1/canvas/p1", # Recommended to be dereferenceable
-          "@type" => "sc:Canvas",
-          "label" => "p. 1", # Page title
-          "height":1000, # Height of the largest image for the page (double if less than 1200)
-          "width":750, # WIdth of the largest image for the page (double if less than 1200)
+          '@id' => 'http://example.org/iiif/book1/canvas/p1', # Recommended to be dereferenceable
+          '@type' => 'sc:Canvas',
+          'label' => 'p. 1', # Page title
+          'height':1000, # Height of the largest image for the page (double if less than 1200)
+          'width':750, # WIdth of the largest image for the page (double if less than 1200)
 
-          "images" => [
+          'images' => [
             {
               # Link from Image to canvas should be included here, as below
-              "@id" => "http://example.org/iiif/book1/annotation/p0001-image", # Recommended to be dereferenced
-              "@type" => "oa:Annotation",
-              "motivation" => "sc:painting",
-              "resource" => {
-                "@id" => "http://example.org/iiif/book1/res/page1.jpg", # IIIF Image URL
-                "@type" => "dctypes:Image",
-                "format" => "image/jpeg",
-                "service" => {
-                  "@context" => "http://iiif.io/api/image/2/context.json",
-                  "@id" => "http://example.org/images/book1-page1", # IIIF Base URL of the image
-                  "profile" => "http://iiif.io/api/image/2/profiles/level2.json"
+              '@id' => 'http://example.org/iiif/book1/annotation/p0001-image', # Recommended to be dereferenced
+              '@type' => 'oa:Annotation',
+              'motivation' => 'sc:painting',
+              'resource' => {
+                '@id' => 'http://example.org/iiif/book1/res/page1.jpg', # IIIF Image URL
+                '@type' => 'dctypes:Image',
+                'format' => 'image/jpeg',
+                'service' => {
+                  '@context' => 'http://iiif.io/api/image/2/context.json',
+                  '@id' => 'http://example.org/images/book1-page1', # IIIF Base URL of the image
+                  'profile' => 'http://iiif.io/api/image/2/profiles/level2.json'
                 },
-                "height":2000,
-                "width":1500
+                'height':2000,
+                'width':1500
               },
-              "on" => "http://example.org/iiif/book1/canvas/p1" # Canvas ID
+              'on' => 'http://example.org/iiif/book1/canvas/p1' # Canvas ID
             }
           ],
-          "otherContent" => [
+          'otherContent' => [
             {
               # Reference to list of other Content resources, _not included directly_
-              "@id" => "http://example.org/iiif/book1/list/p1",
-              "@type" => "sc:AnnotationList"
+              '@id' => 'http://example.org/iiif/book1/list/p1',
+              '@type' => 'sc:AnnotationList'
             }
           ]
         },
         {
-          "@id" => "http://example.org/iiif/book1/canvas/p2",
-          "@type" => "sc:Canvas",
-          "label" => "p. 2"
+          '@id' => 'http://example.org/iiif/book1/canvas/p2',
+          '@type' => 'sc:Canvas',
+          'label' => 'p. 2'
           # ...
         },
         {
-          "@id" => "http://example.org/iiif/book1/canvas/p3",
-          "@type" => "sc:Canvas",
-          "label" => "p. 3"
+          '@id' => 'http://example.org/iiif/book1/canvas/p3',
+          '@type' => 'sc:Canvas',
+          'label' => 'p. 3'
           # ...
         }
       ]
@@ -233,16 +245,6 @@ end
 @manifest_template['sequences'][0]['canvases'][0].delete('otherContent')
 @manifest_template['logo'].delete('service')
 
-#RDF METADATA KEYS
-DC_TITLE = 'http://purl.org/dc/elements/1.1/title'
-DC_DATE = 'http://purl.org/dc/elements/1.1/date'
-BIBO_EDITION = 'http://purl.org/ontology/bibo/edition'
-BIBO_ISSUE = 'http://purl.org/ontology/bibo/issue'
-BIBO_VOLUME = 'http://purl.org/ontology/bibo/volume'
-IANA_FIRST = 'http://www.iana.org/assignments/relation/first'
-IANA_LAST = 'http://www.iana.org/assignments/relation/last'
-HAS_MEMBER = 'http://pcdm.org/models#hasMember'
-HAS_FILE = 'http://pcdm.org/models#hasFile'
 
 def generate_issue_manifest(issue_uri)
   issue = get(issue_uri)
@@ -257,22 +259,22 @@ def generate_issue_manifest(issue_uri)
   #   hash objects should be cloned before updates
   manifest = @manifest_template.clone
 
-  manifest["@id"] = IIIF_MANIFEST_URI + issue_id_encoded + "/manifest"
-  manifest["label"] = issue[DC_TITLE]
-  manifest["metadata"] = Array.new
-  manifest["metadata"].push({'label': 'Date', 'value': issue[DC_DATE]})
-  manifest["metadata"].push({'label': 'Edition', 'value': issue[BIBO_EDITION]})
-  manifest["metadata"].push({'label': 'Volume', 'value': issue[BIBO_VOLUME]})
-  manifest["metadata"].push({'label': 'Issue', 'value': issue[BIBO_ISSUE]})
-  manifest["thumbnail"] = manifest["thumbnail"].clone
-  manifest["thumbnail"]['@id'] = IIIF_IMAGE_URI + first_page_id + '/full/80,100/0/default.jpg'
-  manifest["thumbnail"]['service'] = manifest["thumbnail"]['service'].clone
-  manifest["thumbnail"]['service']['@id'] = IIIF_IMAGE_URI + first_page_id
+  manifest['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + '/manifest'
+  manifest['label'] = issue[DC_TITLE]
+  manifest['metadata'] = Array.new
+  manifest['metadata'].push({'label': 'Date', 'value': issue[DC_DATE]})
+  manifest['metadata'].push({'label': 'Edition', 'value': issue[BIBO_EDITION]})
+  manifest['metadata'].push({'label': 'Volume', 'value': issue[BIBO_VOLUME]})
+  manifest['metadata'].push({'label': 'Issue', 'value': issue[BIBO_ISSUE]})
+  manifest['thumbnail'] = manifest['thumbnail'].clone
+  manifest['thumbnail']['@id'] = IIIF_IMAGE_URI + first_page_id + '/full/80,100/0/default.jpg'
+  manifest['thumbnail']['service'] = manifest['thumbnail']['service'].clone
+  manifest['thumbnail']['service']['@id'] = IIIF_IMAGE_URI + first_page_id
 
   # Populate Sequence properties
   sequence = manifest['sequences'][0].clone
-  sequence["@id"] = IIIF_MANIFEST_URI + issue_id_encoded + '/sequence/normal'
-  sequence["startCanvas"] = IIIF_MANIFEST_URI + issue_id_encoded + "/canvas/" + first_page_id_encoded
+  sequence['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + '/sequence/normal'
+  sequence['startCanvas'] = IIIF_MANIFEST_URI + issue_id_encoded + '/canvas/' + first_page_id_encoded
   manifest['sequences'][0] = sequence
 
   # Get the canvas template and reinitiate canvases array
@@ -299,8 +301,8 @@ def generate_issue_manifest(issue_uri)
       metadata_link = get_described_by_link_header(file_link['@id'])
       file_meta = get(metadata_link)
       mime_type = get_mime_type(file_meta)
-      if (mime_type == "image/tiff")
-        puts "  #{file_link['@id']}"
+      if (mime_type == 'image/tiff')
+        puts "  #{file_link["@id"]}"
         dimensions = get_image_dimensions(file_link['@id'])
         canvas_dimensions = get_canvas_dimension(dimensions)
 
@@ -310,22 +312,22 @@ def generate_issue_manifest(issue_uri)
 
         # Populate Canvas properties
         page_canvas = canvas_template.clone
-        page_canvas['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + "/canvas/" + page_id_encoded
+        page_canvas['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + '/canvas/' + page_id_encoded
         page_canvas['label'] = page[DC_TITLE][0]['@value']
-        page_canvas[:height] = canvas_dimensions["height"]
-        page_canvas[:width] = canvas_dimensions["width"]
+        page_canvas[:height] = canvas_dimensions['height']
+        page_canvas[:width] = canvas_dimensions['width']
 
         # Populate Image properties
         page_image = image_template.clone
-        page_image['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + "/annotation/" + file_id_encoded
+        page_image['@id'] = IIIF_MANIFEST_URI + issue_id_encoded + '/annotation/' + file_id_encoded
         page_image['on'] = page_canvas['@id']
 
         # Populate Resource properties
         page_resource = resource_template.clone
         page_resource['@id'] = IIIF_IMAGE_URI + file_id
         page_resource['format'] = mime_type
-        page_resource[:height] = dimensions["height"]
-        page_resource[:width] = dimensions["width"]
+        page_resource[:height] = dimensions['height']
+        page_resource[:width] = dimensions['width']
 
         # Populate Service properties
         page_service = service_template.clone
