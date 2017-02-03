0750b1cebd860d46212f52d3abf1740e51a105cd
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2017-02-03 09:27:26 -0500

    LIBFCREPO-225. Performance improvements to PCDM manifests.
    Changes:
    - Removed pcdm2manfest.rb
    - Added manifest_helper.rb that retrivies manifest metadata from solr and prepares it for json delivery.
    - Added jb (a jbuilder alternate) gem to deliver the processed solr result document in a json template.
    
    https://issues.umd.edu/browse/LIBFCREPO-225

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'manifests/:id' => 'manifests#show'
+  get 'manifests/:id' => 'manifests#show', defaults: {format: :json}
   get 'manifests' => 'manifests#index'
 end
