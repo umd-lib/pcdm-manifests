03b94e6846ab8e8cc2817a2ab9ee57a55aafde2c
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-05-01 10:05:57 -0400

    LIBIIIF-167: Updating test that were failing.
    https://umd-dit.atlassian.net/browse/LIBIIIF-167

--- test/lib/iiif/fcrepo_test.rb
+++ test/lib/iiif/fcrepo_test.rb
@@ -7,34 +7,36 @@ require 'iiif/fcrepo'
 module IIIF
   module Fcrepo
     # test configuration
-    CONFIG = {
-      fcrepo_url: 'http://localhost:8080/rest/'
-    }.with_indifferent_access.freeze
+    class Item < IIIF::Item
+      CONFIG = {
+        fcrepo_url: 'http://localhost:8080/rest/'
+      }.with_indifferent_access.freeze
+    end
   end
 
   class FcrepoTest < ActiveSupport::TestCase
     test 'simple path' do
-      path = IIIF::Fcrepo::Path.new('a:b:c')
-      assert_equal 'a/b/c', path.expanded
+      path = IIIF::Fcrepo::Path.from_abbreviated('a:b:c')
+      assert_equal 'a/b/c', path.to_s
       assert_equal 'http://localhost:8080/rest/a/b/c', path.to_uri(base_uri: 'http://localhost:8080/rest/')
       assert_equal 'http://localhost:8080/rest/a/b/c', path.to_uri
     end
 
     test 'flat style' do
-      path = IIIF::Fcrepo::Path.new('pcdm::1d8a71b8-7356-4282-8e99-da88f0f997c7')
-      assert_equal 'pcdm/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.expanded
+      path = IIIF::Fcrepo::Path.from_abbreviated('pcdm::1d8a71b8-7356-4282-8e99-da88f0f997c7')
+      assert_equal 'pcdm/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.to_s
       assert_equal 'http://localhost:8080/rest/pcdm/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.to_uri
     end
 
     test 'hierarchical style' do
-      path = IIIF::Fcrepo::Path.new('dc:2021:1::1d8a71b8-7356-4282-8e99-da88f0f997c7')
-      assert_equal 'dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.expanded
+      path = IIIF::Fcrepo::Path.from_abbreviated('dc:2021:1::1d8a71b8-7356-4282-8e99-da88f0f997c7')
+      assert_equal 'dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.to_s
       assert_equal 'http://localhost:8080/rest/dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7', path.to_uri
     end
 
     test 'hierarchical style with sub-path' do
-      path = IIIF::Fcrepo::Path.new('dc:2021:1::1d8a71b8-7356-4282-8e99-da88f0f997c7:m:IQMYmY-R')
-      assert_equal 'dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7/m/IQMYmY-R', path.expanded
+      path = IIIF::Fcrepo::Path.from_abbreviated('dc:2021:1::1d8a71b8-7356-4282-8e99-da88f0f997c7:m:IQMYmY-R')
+      assert_equal 'dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7/m/IQMYmY-R', path.to_s
       assert_equal 'http://localhost:8080/rest/dc/2021/1/1d/8a/71/b8/1d8a71b8-7356-4282-8e99-da88f0f997c7/m/IQMYmY-R', path.to_uri
     end
   end
