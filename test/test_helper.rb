f1a597ff6cc4845313483be69e778c3bbed5f4f6
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-05-03 13:29:47 -0400

    LIBIIIF-169: Adding reporter coverage
    https://umd-dit.atlassian.net/browse/LIBIIIF-169

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@
 
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
